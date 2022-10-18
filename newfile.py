72416075aa2ca7fc80279b06971a6e393a87e70b
Madhackr <115146912+Madhackr@users.noreply.github.com>
2022-10-18 08:09:02 +0600

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,69 @@
+
+import os
+import time
+import sys
+
+
+
+
+os.system("clear")
+
+z="""
+
+              ███╗░░░███╗░█████╗░██████╗░
+              ████╗░████║██╔══██╗██╔══██╗
+              ██╔████╔██║███████║██║░░██║
+              ██║╚██╔╝██║██╔══██║██║░░██║
+              ██║░╚═╝░██║██║░░██║██████╔╝
+              ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═════╝░
+   ╔═══════════════════════════════════════════════╗
+   ║       [✓] TOOL NAME : MAD-X                            ║ 
+   ║       [✓] YOUTUBE : MAD-X                              ║
+   ║       [✓] GITHUB : MAD-x                               ║
+   ║       [✓] EMAIL :madhackr001@gmail.com                 ║
+   ╚═══════════════════════════════════════════════╝
+   """
+print(z)
+print("　")
+print("　")
+print("　")
+
+choice = input("Do You Want to Install cmatrix [Y/N] : ")
+if choice == 'n' :  os.system ("exit")
+
+if choice == 'y' :os.system("apt install cmatrix")
+
+
+os.system("clear")
+
+print(z)
+
+print("　")
+print("　")
+print("　")
+choice = input("Do You Want to Install fire [Y/N] : ")
+if choice == 'n' :  os.system ("exit")
+
+if choice == 'y' :os.system("apt install libcaca")
+
+#oo=os.system("cmatrix")
+
+#if choice =='oo':2
+
+#fire=os.system("cacafire")
+
+#if choice == 'fire': 1
+
+os.system("clear")
+
+print(z)
+
+print("　")
+print("　")
+
+print("[1] Fite")
+
+print("[2] cmatrix")
+print("　")
+
+input("Inter tools namber:")
